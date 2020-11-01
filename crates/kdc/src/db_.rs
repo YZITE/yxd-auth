808d88b6fd3d29dc216a93697f6a6bdcbe9cd317
Erik Zscheile <zseri.devel@ytrizja.de>
2020-11-01 10:21:36 +0100

    KDC/DB: improve dbrpc macro

--- crates/kdc/src/db_.rs
+++ crates/kdc/src/db_.rs
@@ -1,5 +1,8 @@
-use async_oneshot::Sender as ReturnSender;
 use zeroize::Zeroize;
+
+#[derive(Clone, Debug, thiserror::Error)]
+#[error("db connection lost")]
+pub struct DbConnectionLost;
 
 #[derive(Zeroize)]
 #[zeroize(drop)]
@@ -11,12 +14,36 @@ pub struct UserInfo {
 // FIXME: add a way to create users
 // FIXME: add a way to delete users
 
-#[allow(non_camel_case_types)]
-pub enum Message {
-    get_user {
-        ret: ReturnSender<Option<UserInfo>>,
-        user: String,
-    },
+macro_rules! dbrpc {
+    ($($method:ident($($param:ident : $pty:ty),+) -> $retty:ty ;)+) => {
+        #[allow(non_camel_case_types)]
+        pub enum Message {
+            $(
+                $method {
+                    ret: async_oneshot::Sender<$retty>,
+                    $($param: $pty,)+
+                },
+            )+
+        }
+
+        impl crate::ServerStateInner {
+        $(
+            pub(crate) async fn $method(&self, $($param: $pty),+) -> Result<$retty, DbConnectionLost> {
+                let (ret, recv) = async_oneshot::oneshot();
+                let msg = Message::$method {
+                    ret,
+                    $($param,)+
+                };
+                self.db.send(msg).await.map_err(|_| DbConnectionLost)?;
+                recv.await.map_err(|_| DbConnectionLost)
+            }
+        )+
+        }
+    }
+}
+
+dbrpc! {
+    get_user(user: String) -> Option<UserInfo>;
 }
 
 pub fn database_worker(dbpath: std::path::PathBuf, reqs: async_channel::Receiver<Message>) {
@@ -63,36 +90,3 @@ COMMIT;",
     }
 }
 
-#[derive(Clone, Debug, thiserror::Error)]
-#[error("db connection lost")]
-pub struct DbConnectionLost;
-
-impl From<async_oneshot::Closed> for DbConnectionLost {
-    fn from(_: async_oneshot::Closed) -> DbConnectionLost {
-        DbConnectionLost
-    }
-}
-
-impl<T> From<async_channel::SendError<T>> for DbConnectionLost {
-    fn from(_: async_channel::SendError<T>) -> DbConnectionLost {
-        DbConnectionLost
-    }
-}
-
-macro_rules! dbrpc {
-    ($method:ident -> $retty:ty, $($param:ident : $pty:ty),+) => {
-        pub(crate) async fn $method(&self, $($param: $pty),+) -> Result<$retty, DbConnectionLost> {
-            let (ret, recv) = async_oneshot::oneshot();
-            let msg = Message::$method {
-                ret,
-                $($param,)+
-            };
-            self.db.send(msg).await?;
-            recv.await.map_err(DbConnectionLost::from)
-        }
-    }
-}
-
-impl crate::ServerStateInner {
-    dbrpc!(get_user -> Option<UserInfo>, user: String);
-}
